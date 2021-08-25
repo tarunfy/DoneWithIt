819c6b7211fc5e23c3739354217af0dc452b0976
tarun2506 <tarunsharma8920@gmail.com>
2021-08-25 21:34:49 +0530

    changes

--- App.js
+++ App.js
@@ -1,19 +1,29 @@
 import React from "react";
-import { View, StyleSheet, Text, Platform } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Text,
+  Platform,
+  SafeAreaView,
+  StatusBar,
+} from "react-native";
 import WelcomeScreen from "./app/screens/welcomeScreen";
 import ViewImageScreen from "./app/screens/viewImageScreen";
 import { Entypo } from "@expo/vector-icons";
 import AppButton from "./app/components/AppButton";
 import Card from "./app/components/Card";
 import ListingDetailScreen from "./app/screens/ListingDetailScreen";
+import MessagesScreen from "./app/screens/MessagesScreen";
 export default function App() {
-  return <ViewImageScreen />;
+  return (
+    <SafeAreaView style={styles.container}>
+      <MessagesScreen />
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "lightgrey",
-    padding: 20,
-    paddingTop: 60,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
---
+++ app/screens/MessagesScreen.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { FlatList, View, StyleSheet } from "react-native";
+import ListItem from "../components/ListItem";
+
+const messages = [
+  {
+    id: 1,
+    title: "T1",
+    description: "D1",
+    image: require("../assets/mosh.jpg"),
+  },
+  {
+    id: 2,
+    title: "T2",
+    description: "D2",
+    image: require("../assets/mosh.jpg"),
+  },
+  {
+    id: 3,
+    title: "T3",
+    description: "D3",
+    image: require("../assets/mosh.jpg"),
+  },
+];
+
+function MessagesScreen(props) {
+  return (
+    <View style={styles.listContainer}>
+      <FlatList
+        data={messages}
+        keyExtractor={(message) => message.id.toString()}
+        renderItem={({ item }) => (
+          <ListItem
+            title={item.title}
+            subtitle={item.description}
+            image={item.image}
+          />
+        )}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  listContainer: {
+    marginTop: 30,
+  },
+});
+
+export default MessagesScreen;
