9f98ed8426d065bdb9627df6fb7b09090b8dc62b
tarun2506 <tarunsharma8920@gmail.com>
2021-08-26 19:14:07 +0530

    Listing screen ready

--- App.js
+++ App.js
@@ -18,6 +18,7 @@ import Screen from "./app/components/Screen";
 import Icon from "./app/components/Icon";
 import ListItem from "./app/components/ListItem";
 import AccountScreen from "./app/screens/AccountScreen";
+import ListingScreen from "./app/screens/ListingScreen";
 export default function App() {
-  return <AccountScreen />;
+  return <ListingScreen />;
 }
---
+++ app/screens/ListingScreen.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { FlatList, StyleSheet } from "react-native";
+import Card from "../components/Card";
+import Screen from "../components/Screen";
+import colors from "../config/colors";
+
+const listings = [
+  {
+    id: 1,
+    title: "Red jacket for sale",
+    price: 100,
+    image: require("../assets/jacket.jpg"),
+  },
+  {
+    id: 2,
+    title: "Couch in great condition",
+    price: 2000,
+    image: require("../assets/couch.jpg"),
+  },
+];
+
+function ListingScreen() {
+  return (
+    <Screen style={styles.screen}>
+      <FlatList
+        data={listings}
+        keyExtractor={(listing) => listing.id.toString()}
+        renderItem={({ item }) => (
+          <Card
+            title={item.title}
+            subtitle={`$${item.price}`}
+            image={item.image}
+          />
+        )}
+      />
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 20,
+    backgroundColor: colors.grey,
+  },
+});
+
+export default ListingScreen;
