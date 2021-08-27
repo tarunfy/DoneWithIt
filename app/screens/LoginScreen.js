de724960e008ff75502d54d4ba86d1586d0e2553
tarun2506 <tarunsharma8920@gmail.com>
2021-08-27 18:16:04 +0530

    using formik

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,33 +1,41 @@
-import React, { useState } from "react";
+import React from "react";
 import { Image, StyleSheet } from "react-native";
+import { Formik } from "formik";
 import AppButton from "../components/AppButton";
 import AppTextInput from "../components/AppTextInput";
 import Screen from "../components/Screen";
 function LoginScreen() {
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
   return (
     <Screen style={styles.container}>
       <Image source={require("../assets/logo-red.png")} style={styles.logo} />
-      <AppTextInput
-        placeholder="Email"
-        icon="email"
-        autoCapitalize="none"
-        autoCorrect={false}
-        keyboardType="email-address"
-        callback={(text) => setEmail(text)}
-        bool={false}
-      />
-      <AppTextInput
-        multiline={false}
-        placeholder="password"
-        autoCapitalize="none"
-        autoCorrect={false}
-        icon="lock"
-        bool={true}
-        callback={(text) => setPassword(text)}
-      />
-      <AppButton title="Login" onPress={() => console.log(email, password)} />
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        onSubmit={(values) => console.log(values)}
+      >
+        {({ handleChange, handleSubmit }) => (
+          <>
+            <AppTextInput
+              placeholder="Email"
+              icon="email"
+              autoCapitalize="none"
+              autoCorrect={false}
+              keyboardType="email-address"
+              callback={handleChange("email")}
+              bool={false}
+            />
+            <AppTextInput
+              multiline={false}
+              placeholder="password"
+              autoCapitalize="none"
+              autoCorrect={false}
+              icon="lock"
+              bool={true}
+              callback={handleChange("password")}
+            />
+            <AppButton title="Login" onPress={handleSubmit} />
+          </>
+        )}
+      </Formik>
     </Screen>
   );
 }
