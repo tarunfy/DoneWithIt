0585cc4416d06cc190bcbf5771b161f06856fc0b
tarun2506 <tarunsharma8920@gmail.com>
2021-08-27 15:33:46 +0530

    modal

--- app/components/AppPicker.js
+++ app/components/AppPicker.js
@@ -1,27 +1,47 @@
-import React from "react";
-import { Text, View, StyleSheet, Platform, TextInput } from "react-native";
+import React, { useState } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Platform,
+  TextInput,
+  TouchableWithoutFeedback,
+  Modal,
+  Button,
+} from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import colors from "../config/colors";
 import defaultStyles from "../config/defaultStyles";
 import AppText from "./AppText";
+import Screen from "./Screen";
 function AppPicker({ icon, placeholder }) {
+  const [modalVisible, setModalVisible] = useState(false);
   return (
-    <View style={styles.container}>
-      {icon && (
-        <MaterialCommunityIcons
-          style={styles.icon}
-          name={icon}
-          size={20}
-          color={defaultStyles.colors.lightDark}
-        />
-      )}
-      <AppText style={styles.text}>{placeholder}</AppText>
-      <MaterialCommunityIcons
-        name="chevron-down"
-        size={20}
-        color={defaultStyles.colors.lightDark}
-      />
-    </View>
+    <>
+      <TouchableWithoutFeedback onPress={() => setModalVisible(true)}>
+        <View style={styles.container}>
+          {icon && (
+            <MaterialCommunityIcons
+              style={styles.icon}
+              name={icon}
+              size={20}
+              color={defaultStyles.colors.lightDark}
+            />
+          )}
+          <AppText style={styles.text}>{placeholder}</AppText>
+          <MaterialCommunityIcons
+            name="chevron-down"
+            size={20}
+            color={defaultStyles.colors.lightDark}
+          />
+        </View>
+      </TouchableWithoutFeedback>
+      <Modal visible={modalVisible} animationType="slide">
+        <Screen>
+          <Button title="close" onPress={() => setModalVisible(false)} />
+        </Screen>
+      </Modal>
+    </>
   );
 }
 
